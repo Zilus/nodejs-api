0f8d60883678dcc1f728786488373595c4b3e2b5
Oscar Azpeitia <neozilus@gmail.com>
2018-06-14 21:45:23 -0500

    Pets schema

---
+++ models/pets.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const mongooseStringQuery = require('mongoose-string-query');
+const timestamps = require('mongoose-timestamp');
+
+const PetSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		tag: {
+			type: String,
+			required: false,
+		},
+	},
+	{ minimize: false },
+);
+
+PetSchema.plugin(timestamps);
+PetSchema.plugin(mongooseStringQuery);
+
+const Pet = mongoose.model('Pet', PetSchema);
+module.exports = Pet;  
